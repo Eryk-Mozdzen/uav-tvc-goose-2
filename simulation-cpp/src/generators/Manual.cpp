e21a1f8f0e3626080222a58320b02b4c0a2285b7
Eryk Możdżeń <ermozdzen@gmail.com>
2024-02-16 20:04:31 +0100

    remove derivative from manual controll

--- simulation-cpp/src/generators/Manual.cpp
+++ simulation-cpp/src/generators/Manual.cpp
@@ -1,6 +1,6 @@
 #include <drake/systems/framework/diagram_builder.h>
 #include <drake/systems/primitives/integrator.h>
-#include <drake/systems/primitives/discrete_derivative.h>
+#include <drake/systems/primitives/constant_vector_source.h>
 #include <drake/systems/primitives/multiplexer.h>
 
 #include "Manual.h"
@@ -12,14 +12,13 @@ Manual::Manual() {
 
     auto gamepad = builder.AddSystem<Gamepad>();
     auto integrator = builder.AddSystem<drake::systems::Integrator>(4);
-    auto derivative = builder.AddSystem<drake::systems::DiscreteDerivative>(4, 0.01);
+    auto zero = builder.AddSystem<drake::systems::ConstantVectorSource>(Eigen::Vector4d(0, 0, 0, 0));
     auto mux = builder.AddSystem<drake::systems::Multiplexer>(std::vector<int>{4, 4, 4});
 
     builder.Connect(gamepad->get_output_port(), integrator->get_input_port());
-    builder.Connect(gamepad->get_output_port(), derivative->get_input_port());
-    builder.Connect(gamepad->get_output_port(), mux->get_input_port(1));
     builder.Connect(integrator->get_output_port(), mux->get_input_port(0));
-    builder.Connect(derivative->get_output_port(), mux->get_input_port(2));
+    builder.Connect(gamepad->get_output_port(), mux->get_input_port(1));
+    builder.Connect(zero->get_output_port(), mux->get_input_port(2));
 
     builder.ExportOutput(mux->get_output_port(), "trajectory");
 
